71f67b1d07e359d350e8eeeb0d51b8a660b55ecf
rajinikanth47 <geeni.mca@gmail.com>
2019-12-02 12:02:36 +0800

    Add recyclerview and empty holder Enable data binding

--- app/src/main/java/com/spdigital/weatherapp/MainActivity.kt
+++ app/src/main/java/com/spdigital/weatherapp/MainActivity.kt
@@ -2,18 +2,24 @@ package com.spdigital.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.spdigital.weatherapp.databinding.ActivityMainBinding
 import com.spdigital.weatherapp.util.LocationQueue
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         btnSearch.setOnClickListener{
             val searchLocation = etSearchLocation.text.toString()
             LocationQueue.addLocationToQueue(searchLocation)
+
+            binding.hasSavedLocations = false
 
             println("Location List ${LocationQueue.getLocationQueueAsList()}")
         }
